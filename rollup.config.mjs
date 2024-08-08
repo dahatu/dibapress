cc574d01540a95a623580bcc3d17f35b06a5703c
Bahman World <bahman.world@gmail.com>
2024-08-08 23:57:17 +0330

    update: Rollup config updated

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,11 +3,12 @@ import { globSync } from "glob";
 import treser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import image from "@rollup/plugin-image";
+import copy from "rollup-plugin-copy";
 import path from "path";
 import { fileURLToPath } from "url";
 
 export default defineConfig({
-  input: Object.fromEntries( 
+  input: Object.fromEntries(
     globSync(["./src/**/*.ts", "./src/**/*.tsx"]).map((file) => [
       path.relative(
         "src",
@@ -18,10 +19,22 @@ export default defineConfig({
   ),
   output: {
     dir: "./dist",
-    format: "esm"
+    format: "esm",
   },
   onwarn: (e) => {
     console.warn("BAHMAN WARN:", e.cause);
   },
-  plugins: [treser(), typescript(), image({ dom: true })],
+  plugins: [
+    treser(),
+    typescript(),
+    image({ dom: true }),
+    copy({
+      targets: [
+        {
+          src: ["./package.json", "./LISANCE", "./CHANGELOG.md", "README.md"],
+          dest: "dist",
+        },
+      ],
+    }),
+  ],
 });
